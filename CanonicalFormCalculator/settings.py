80dd61e47d1fcd7611fe65711f6664cbba4484de
Joel home <joelsongbae@hotmail.com>
2023-02-19 21:29:51 -0500

    cors

--- CanonicalFormCalculator/settings.py
+++ CanonicalFormCalculator/settings.py
@@ -43,11 +43,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders'
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
